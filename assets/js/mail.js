449cf1faaa9d6c28c39a38dcad80a26ce9a77cef
Matthias <matthias@ideasoncanvas.com>
2017-02-26 20:46:26 +0000

    Initial Commit

---
+++ assets/js/mail.js
@@ -0,0 +1,7 @@
+function em1() {
+    var c = "nbuuijbtAjefbtpodbowbt/dpn";
+    var addr = "mailto:";
+    for (var i = 0; i < c.length; i++)
+        addr += String.fromCharCode(c.charCodeAt(i) - 1);
+    window.location.href = addr;
+}
